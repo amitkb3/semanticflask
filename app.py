91461be0db763d27dbc8986aa402b5a54b183a2e
Amit Bothra <amit.bothra@outlook.com>
2020-02-15 17:13:40 -0600

    added static artifacts

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/concept')
+def concept():
+  data = { "image":"lion.jpg","english":"Lion","hindi":"सिंह"}
+  return render_template('concept.html', data=data)
